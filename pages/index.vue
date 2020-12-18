530d57f8538ae909ed405ffae5defdd1a1d83d5b
Alton <alton_ong@live.com.sg>
2020-12-18 15:07:29 +1100

    Initial Commit
    initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="container">
+    <nuxt-link to="/users">Users</nuxt-link>
+    <!-- <button @click="$router.push('/products')">Go to products</button> -->
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input v-model="userId" type="text" />
+    <button @click="onLoadUser">Load user</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: '',
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userId)
+    },
+  },
+}
+</script>
+<style></style>
